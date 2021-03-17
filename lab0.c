422c288d2440c433d09becf2c69472699cdb414b
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-03-17 19:26:49 +0530

    lab 0

---
+++ lab0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("\nHello, World\n");
+
+    int x = 10;
+    int y = 30;
+    printf("%d\n", x + y);
+    printf("%d\n", x - y);
+    printf("%d\n", x * y);
+
+    return 0;
+}
