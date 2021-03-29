f6bcb3337557127744b98ce3f9bde0d47ca534aa
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-03-30 03:39:04 +0530

    BITSID

---
+++ HomeWorkExcercise/BITSID.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int year, branch, id;
+    char p = 'P', s = 'S', a = 'A', campus;
+    printf("Enter BITS ID:");
+    scanf("%4d%c%d%c%c%4d%c", &year, &a, &branch, &p, &s, &id, &campus);
+    if (campus == 'G')
+    {
+        printf("Your BITS email is: ");
+        printf("f%d%d@goa.bits-pilani.ac.in\n", year, id);
+    }
+    else if (campus == 'P')
+    {
+        printf("Your BITS email is: ");
+        printf("f%d%d@pilani.bits-pilani.ac.in\n", year, id);
+    }
+    else if (campus == 'H')
+    {
+        printf("Your BITS email is: ");
+        printf("f%d%d@hyd.bits-pilani.ac.in\n", year, id);
+    }
+    else
+    {
+        printf("Enter valid id\n");
+    }
+}
