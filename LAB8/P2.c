80ba01c3629f49524692bd9f7679bf2ff24773b7
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-06-08 17:40:26 +0530

    P2

--- LAB8/P2.c
+++ LAB8/P2.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
-int main(){
-    
+typedef int Arr1D[2];
+typedef Arr1D Arr2D[3];
+
+void set(int *x)
+{
+    *x = 1;
+}
+
+void reset(int *x)
+{
+    *x = -1;
+}
+
+int main()
+{
+    Arr2D A;
+    int i = 0, j = 0;
+
+    for (int a = 0; a < 3; a++)
+    {
+        for (int b = 0; b < 2; b++)
+        {
+            reset(&A[a][b]);
+        }
+    }
+
+    printf("Enter i: ");
+    scanf(" %d", &i);
+
+    printf("Enter j: ");
+    scanf(" %d", &j);
+
+    set(&A[i-1][j-1]);
+
+    for (int a = 0; a < 3; a++)
+    {
+        for (int b = 0; b < 2; b++)
+        {
+            printf("%d ", A[a][b]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
